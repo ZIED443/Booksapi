af87ee11f9074e90e7f8abfe630665096109fb4b
ZIED443 <ziedjmal99@gmail.com>
2023-09-27 22:16:44 +0100

    test prep

--- books/models.py
+++ books/models.py
@@ -5,34 +5,27 @@ import datetime
 from datetime import datetime
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-
 class CustomUser(BaseUserManager):
-
-    def create_superuser(self,  password, email, user_name , **other_fields):   
+    def create_superuser(self, email, user_name, password=None, **other_fields):
         other_fields.setdefault('is_staff', True)
         other_fields.setdefault('is_superuser', True)
         other_fields.setdefault('is_active', True)
-        
+
         if other_fields.get('is_staff') is not True:
-            raise ValueError(
-                'Superuser must be assigned to is_staff=True.')
+            raise ValueError('Superuser must be assigned to is_staff=True.')
         if other_fields.get('is_superuser') is not True:
-            raise ValueError(
-                'Superuser must be assigned to is_superuser=True.')
+            raise ValueError('Superuser must be assigned to is_superuser=True.')
 
-        return self.create_user(   password, email, user_name , **other_fields)
+        return self.create_user(email, user_name, password=password, **other_fields)
 
-    def create_user(self,  password, email, user_name , **other_fields):
-
-        
+    def create_user(self, email, user_name, password=None, **other_fields):
         if not email:
-            raise ValueError(_('You need to provide an email adress  '))
+            raise ValueError(_('You need to provide an email address'))
 
-       
-        user = self.model( password = password  , email = email ,user_name=user_name, **other_fields)
+        user = self.model(email=email, user_name=user_name, password=password , **other_fields)
         user.set_password(password)
         user.save()
-        return user          
+        return user       
   
 
 class User(AbstractBaseUser, PermissionsMixin):  
--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,85 @@
-from django.test import TestCase
 
-# Create your tests here.
+import pytest
+from django.utils import timezone
+from .models import CustomUser, User, Auhor, Book, Bookreservation
+
+@pytest.mark.django_db
+def test_custom_user_create_user():
+    user = User.objects.create_user(
+        password="testpassword",
+        email="test@example.com",
+        user_name="testuser",
+    )
+    assert user.email == "test@example.com"
+    assert user.user_name == "testuser"
+    assert user.is_staff is False
+    assert user.is_active is True
+    assert user.check_password("testpassword")
+
+@pytest.mark.django_db
+def test_custom_user_create_superuser():
+    user = User.objects.create_superuser(
+        password="superpassword",
+        email="super@example.com",
+        user_name="superuser",
+    )
+    assert user.email == "super@example.com"
+    assert user.user_name == "superuser"
+    assert user.is_staff is True
+    assert user.is_superuser is True
+    assert user.is_active is True
+    assert user.check_password("superpassword")
+
+@pytest.mark.django_db
+def test_user_str_representation():
+    user = User.objects.create(
+        email="user@example.com",
+        user_name="testuser",
+    )
+    assert str(user) == "user@example.com"
+
+@pytest.mark.django_db
+def test_author_model():
+    author = Auhor.objects.create(
+        name="Test Author",
+        birthday=timezone.now(),
+    )
+    assert author.name == "Test Author"
+
+@pytest.mark.django_db
+def test_book_model():
+    author = Auhor.objects.create(
+        name="Test Author",
+        birthday=timezone.now(),
+    )
+    book = Book.objects.create(
+        author=author,
+        date_release=timezone.now(),
+        name="Test Book",
+    )
+    assert book.name == "Test Book"
+    assert book.author == author
+
+@pytest.mark.django_db
+def test_bookreservation_model():
+    user = User.objects.create(
+        email="user@example.com",
+        user_name="testuser",
+    )
+    author = Auhor.objects.create(
+        name="Test Author",
+        birthday=timezone.now(),
+    )
+    book = Book.objects.create(
+        author=author,
+        date_release=timezone.now(),
+        name="Test Book",
+    )
+    reservation = Bookreservation.objects.create(
+        user=user,
+        book=book,
+        date1=timezone.now(),
+        date2=timezone.now(),
+    )
+    assert reservation.user == user
+    assert reservation.book == book
