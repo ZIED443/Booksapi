128bd9669d9d6dde1bf0cc5c0a754088f3c92884
ZIED443 <ziedjmal99@gmail.com>
2025-01-07 18:59:10 +0100

    correction settigns

--- djangocont/booksproject/settings.py
+++ djangocont/booksproject/settings.py
@@ -11,21 +11,27 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+from dotenv import load_dotenv
 
+load_dotenv() 
+print (os.getenv("SECRET_KEY"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--@c#j=c%@6ghd9akpm55y5wv*5flu+uefn8)rn)n(dmi-7asnj'
+
+SECRET_KEY =os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
+if not DEBUG:
+    ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -82,10 +88,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'apidb',
-        'USER': 'zied',
-        'PASSWORD': 'password',
+        'USER': os.getenv('db_user'),
+        'PASSWORD': os.getenv('db_password'),
         'HOST': 'db',  
-        'PORT': '5432',
+        'PORT': os.getenv('db_port'),
     }
 }
 
@@ -135,8 +141,8 @@ AUTH_USER_MODEL = 'books.User'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.office365.com'  
-EMAIL_PORT = 587  
+EMAIL_PORT = os.getenv('email_port')  
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ziedjmal@hotmail.com'  # Your Hotmail email address
-EMAIL_HOST_PASSWORD = 'ziedzied1999@'  # Your Hotmail email password
-DEFAULT_FROM_EMAIL = 'ziedjmal@hotmail.com'  # Default "From" email address
+EMAIL_HOST_USER =os.getenv('email_login')  # Your Hotmail_login
+EMAIL_HOST_PASSWORD = os.getenv('email_password')  # Your Hotmail email password
+DEFAULT_FROM_EMAIL = os.getenv('email_login')  # Default "From" email address
